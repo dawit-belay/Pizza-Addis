b4bc9997af55fd67ebc26b4f854bd2ab10b7ec12
dawit belay <dawitalhabesha@gmail.com>
2025-06-01 12:34:41 +0300

    adding api

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ function App() {
   return (
     <div>
       <h1>Pizza Addis - order Now</h1>
-      <Pizza name = "Pepperoni" description="pep, cheese, n stuff"/>
-      <Pizza name = "Hawaiian" description="ham, pineapple, n stuff"/>
-      <Pizza name = "pepperoni" description="french fries, hot dogs, n stuff"/>
+      <Pizza
+        name = "Pepperoni" 
+        description="pep, cheese, n stuff" 
+        image ={"/public/pizzas/pepperoni.webp"}
+       />
+      <Pizza 
+        name = "Hawaiian" 
+        description="ham, pineapple, n stuff" 
+        image ={"/public/pizzas/Hawaiian.webp"}
+      />
+      <Pizza 
+        name = "pepperoni" 
+        description="french fries, hot dogs, n stuff" 
+        image =  {"/public/pizzas/big_meat.webp"}
+      />
     </div>
   )
 }
