62bcf67143046579596305a1e7226a321d77dfec
dawit belay <dawitalhabesha@gmail.com>
2025-06-01 09:59:55 +0300

    first pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import './App.css'
+import Pizza from './Pizza'
 
 function App() {
+  return (
+    <div>
+      <h1>Pizza Addis - order Now</h1>
+      <Pizza name = "Pepperoni" description="pep, cheese, n stuff"/>
+      <Pizza name = "Hawaiian" description="ham, pineapple, n stuff"/>
+      <Pizza name = "pepperoni" description="french fries, hot dogs, n stuff"/>
+    </div>
+  )
 }
 
 export default App
